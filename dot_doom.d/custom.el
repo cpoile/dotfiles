95c5e3a0ad6f50eb542c65c231803c09f178e7ef
Christopher Poile <cpoile@gmail.com>
2025-03-02 11:19:49 -0500

    Update .doom.d/custom.el Update .zshrc

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -3,61 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ediff-current-diff-A ((t (:extend t :background "#3f575a"))))
- '(ediff-current-diff-B ((t (:inherit ediff-current-diff-A :background "#304946"))))
- '(ediff-even-diff-B ((t (:inherit ediff-even-diff-A :background "#2e4542"))))
- '(ediff-fine-diff-A ((t (:extend t :background "#304946" :weight bold))))
- '(next-error-message ((t (:extend t)))))
+ )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((compilation-search-path "~/git/elixir/in_action/")
-     (compilation-search-path "/home/chris/git/elixir/in_action/test/todo/")
-     (compilation-search-path "/home/chris/git/elixir/in_action/test")
-     (compilation-search-path "/home/chris/git/elixir/in_action")
-     (compilation-search-path "/home/chris/git/elixir/in_action/")
-     (custom-run-test-cmd . "/home/chris/git/elixir/in_action/run_tests.sh")
-     (custom-run-test-cmd
-      . "/home/chris/git/elixir/in_action/run_in_pwd.sh mix test")
-     (custom-run-test-cmd . "/home/chris/git/elixir/in_action/mix test")
-     (default-compile-cmd
-      . "jai c:/Users/Chris/git/jai/handmade_jai/build_handmade.jai")
-     (defualt-compile-cmd
-      . "jai c:/Users/Chris/git/jai/handmade_jai/build_handmade.jai")
-     (compile-all-cmd . "c:/Users/Chris/git/jai/handmade_jai/make.bat")
-     (file-to-compile-cmd
-      ("win32_handmade.jai" compile
-       "jai c:/Users/Chris/git/jai/handmade_jai/build_win32_handmade.jai"))
-     (custom-compile-cmd
-      . "jai c:/Users/Chris/git/jai/handmade_jai/build_handmade.jai")
-     (custom-run-test-cmd
-      . "c:/Users/Chris/git/jai/scratch/build/scratch.exe test")
-     (custom-run-all-cmd . "c:/Users/Chris/git/jai/scratch/build/scratch.exe")
-     (custom-compile-cmd . "c:/Users/Chris/git/jai/scratch/make.bat")
-     (custom-run-test-cmd
-      . "odin test c:/Users/Chris/git/odin/writing_an_interpreter/src/ -all-packages -define:ODIN_TEST_FANCY=false -debug -o:minimal -out:c:/Users/Chris/git/odin/writing_an_interpreter/build/test.exe")
-     (custom-run-all-cmd
-      . "c:/Users/Chris/git/odin/writing_an_interpreter/build/interpreter.exe")
-     (custom-compile-cmd
-      . "c:/Users/Chris/git/odin/writing_an_interpreter/make.bat")
-     (custom-run-test-cmd
-      . "odin test c:/Users/Chris/git/odin/handmade_hero/src/ -all-packages -define:ODIN_TEST_FANCY=false -debug -o:minimal -out:c:/Users/Chris/git/odin/handmade_hero/build/test.exe")
-     (custom-run-all-cmd
-      . "c:/Users/Chris/git/odin/handmade_hero/build/handmade.exe")
-     (custom-compile-cmd . "c:/Users/Chris/git/odin/handmade_hero/make.bat")
-     (custom-run-test-cmd
-      . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe test")
-     (custom-run-all-cmd
-      . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe")
-     (custom-compile-cmd . "c:/Users/Chris/git/jai/handmade_jai/make.bat")
-     (custom-run-test-cmd
-      . "c:/Users/Chris/git/jai/writing-an-interpreter/build/main.exe test")
-     (custom-run-all-cmd
-      . "c:/Users/Chris/git/jai/writing-an-interpreter/build/main.exe")
-     (custom-compile-cmd
-      . "c:/Users/Chris/git/jai/writing-an-interpreter/make.bat")
-     (mangle-whitespace . t))))
+   '((custom-run-test-cmd
+      . "/home/chris/git/elixir/codecrafters-redis-elixir/run_tests.sh")
+     (custom-run-test-cmd . "/home/chris/git/elixir/in_action/run_tests.sh"))))
 (put 'projectile-ripgrep 'disabled nil)
